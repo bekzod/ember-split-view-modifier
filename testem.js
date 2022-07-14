d53c7070c38b1f37b29d27cfed3cbb79c2fc02d6
bek <bekzod@me.com>
2022-07-14 15:40:05 +0500

    test pr

--- testem.js
+++ testem.js
@@ -3,6 +3,7 @@
 module.exports = {
   test_page: "tests/index.html?hidepassed",
   disable_watching: true,
+  parallel: -1,
   launch_in_ci: ["Chrome"],
   launch_in_dev: ["Chrome"],
   browser_start_timeout: 120,
